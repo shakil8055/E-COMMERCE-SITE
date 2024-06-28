11fbd0cda2c52b87924342daef0d431f772501c1
shakil8055 <mohamedhshakil883@gmail.com>
2024-06-28 20:18:26 +0530

    commit

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -8,17 +8,17 @@ function TopBar({ cart, setCart }) {
           ALFA GARMENTS
         </a>
         <button
-          className="navbar-toggler collapsed"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
-          aria-expanded="true"
+          aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="responsive-navbar-nav">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#!">
